ef37ce0cbb0cc9ef5b5710053246b9c8df283f47
yanisapths <yanisa21@live.com>
2025-06-04 22:06:36 +0700

    chore: add redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/(.*)",
+        has: [{ type: "host", value: "www.thaicertifi.com" }],
+        destination: "https://thaicertifi.com/:1",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
