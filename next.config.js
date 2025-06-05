15b3746deda29769e8e2025387ca91eb533af693
yanisapths <yanisa21@live.com>
2025-06-05 07:34:27 +0700

    chore: remove redirect

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/(.*)",
-        has: [{ type: "host", value: "www.thaicertifi.com" }],
-        destination: "https://thaicertifi.com/:1",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
